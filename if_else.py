f429cba9d71e7b7cf28eefc1bd51bade1c06ae8d
UrvashiBhadaniya <145823903+UrvashiBhadaniya@users.noreply.github.com>
2023-09-23 10:24:20 +0530

    Add files via upload

---
+++ if_else.py
@@ -0,0 +1,13 @@
+#odd_even
+x = int(input("Enter any Number: "))
+if x%2==0:
+    print("Number is even")
+else:
+    print("Number is odd")
+
+#constant_vowel
+x = str(input("Enter any letter: "))
+if x=='a' or x=='e' or x=='i' or x=='o' or x=='u':
+    print("Number is Vowel")
+else:
+    print("Number is Constant")
